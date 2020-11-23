66cb632c82b86d0863f7e014edd63a9594dcb636
Oleg Nenashev <o.v.nenashev@gmail.com>
2020-11-23 12:35:37 +0100

    Remove unused import

--- plugin-management-cli/src/main/java/io/jenkins/tools/pluginmanager/cli/VersionNumberHandler.java
+++ plugin-management-cli/src/main/java/io/jenkins/tools/pluginmanager/cli/VersionNumberHandler.java
@@ -5,7 +5,6 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.OptionDef;
 import org.kohsuke.args4j.spi.OneArgumentOptionHandler;
-import org.kohsuke.args4j.spi.Parameters;
 import org.kohsuke.args4j.spi.Setter;
 
 // TODO(oleg_nenashev): Move to the VersionNumber lib?
