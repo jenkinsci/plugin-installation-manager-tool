1ed7840b235043c12628c9a79da719bf4ebc1765
hp77 <hp77-creator@users.noreply.github.com>
2021-01-26 14:27:56 +0530

    Add directory address in DirectoryCreationException Message (#273)
    * Add directory address in DirectoryCreationException Message
    
    * Apply suggestions from code review
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- plugin-management-library/src/main/java/io/jenkins/tools/pluginmanager/impl/PluginManager.java
+++ plugin-management-library/src/main/java/io/jenkins/tools/pluginmanager/impl/PluginManager.java
@@ -192,7 +192,7 @@ public class PluginManager {
         try {
             Files.createDirectories(pluginDir.toPath());
         } catch (IOException e) {
-            throw new DirectoryCreationException("Unable to create plugin directory", e);
+            throw new DirectoryCreationException(String.format("Unable to create plugin directory: '%s', supply a directory with -d <your-directory>", pluginDir), e);
         }
     }
 
