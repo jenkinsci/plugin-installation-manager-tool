e3bb6cb33b0289a1cb5966470b41c1af2e4aa889
Mukund B Srinivas <mukundsrinivasb@hotmail.com>
2023-03-13 08:00:04 +0000

    Well some more checkstyle solved

--- plugin-management-library/src/main/java/io/jenkins/tools/pluginmanager/impl/PluginManager.java
+++ plugin-management-library/src/main/java/io/jenkins/tools/pluginmanager/impl/PluginManager.java
@@ -22,7 +22,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
-
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
